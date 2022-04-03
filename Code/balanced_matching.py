76eaa9f196df96f90bded5758da8bad7ec08363d
danielsloredo <daniel.sloredo@gmail.com>
2022-04-03 13:56:30 -0400

    new code folder

---
+++ Code/balanced_matching.py
@@ -0,0 +1,22 @@
+import numpy as np
+import numpy as np
+import networkx as nx
+import random as ra
+import matplotlib.pyplot as plt
+import math 
+
+g = nx.Graph()
+
+for i in range(10):
+    g.add_node(i)
+
+print(g.nodes())
+
+
+for i in range(4):
+    for j in range(5):
+        g.add_edge(i,j+4)
+
+print(g.edges())
+
+print('code succesfull')
