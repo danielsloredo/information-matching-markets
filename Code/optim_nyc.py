ddc36e20f23645a01e32297ddf83088e8d686513
danielsloredo <daniel.sloredo@gmail.com>
2022-09-16 14:01:27 -0400

    functions to check if matching is pareto optimal

--- Code/optim_nyc.py
+++ Code/optim_nyc.py
@@ -943,7 +943,8 @@ def mc_simulations_bad_edges(Delta, sublist, additions, n_students, n_schools, i
 
 def find_cycles(unmatched_students, unmatched_schools, next_student_choice, next_school_choice, student_preferences, school_preferences, student_match):
     potential_match_student = np.copy(student_match)
-    potential_match_school = [-1] * 10
+    n_schools = len(next_school_choice)
+    potential_match_school = [-1] * n_schools
     candidate_students = []
 
     for student in unmatched_students:
@@ -1032,4 +1033,244 @@ def top_trading_cycles(n_students, n_schools, student_preferences, school_prefer
                     unmatched_students.remove(student)
                     unmatched_schools.remove(school)                  
     return student_match, school_match
+
+def simulation_matching_increase_preferences_ttc(Delta, k, n_students, n_schools, additions):
+    '''
+    Simulates the matching outcome under diferent preference list sizes where we only add new preferences
+    by using the ttc algorithm.
+    '''
+    student_f_pref, school_f_pref = marriage_market_preference_lists(n_students, n_schools)
+
+    student_pre = {}
+    school_pre = {}
+    student_M = {}
+    school_M = {}
+    
+    student_pre[k], school_pre[k] = restricted_market(k, student_f_pref, school_f_pref)
+    student_M[k], school_M[k] = top_trading_cycles(n_students, n_schools, student_pre[k], school_pre[k])
+    
+    for j in range(1,additions+1):
+        k_prev = k
+        k = k + Delta
+        print('working on sublist size: ' + str(k))
+        student_pre[k], school_pre[k] = increase_preference_sublist(Delta, student_pre[k_prev], school_pre[k_prev], student_f_pref, school_f_pref)
+        student_M[k], school_M[k] = top_trading_cycles(n_students, n_schools, student_pre[k], school_pre[k])
+        
+    return student_M, school_M, student_f_pref, school_f_pref
+
+
+def mc_simulations_utility_ttc(Delta, sublist, additions, n_students, n_schools, iterations):
+    '''
+    Function to simulate the behaviour of different utility functions on the stable match
+    outcome with different sublist sizes.
+    '''
+    beg = sublist + Delta
+    end = sublist+Delta*additions + Delta
+    
+    average_nash_welfare_students = {x : 0 for x in range(sublist, end, Delta)}
+    average_nash_welfare_schools = {x : 0 for x in range(sublist, end, Delta)}
+
+    average_leontief_utility = {x : 0 for x in range(sublist, end, Delta)} 
+    average_cobb_stone_utility = {x : 0 for x in range(sublist, end, Delta)}
+    average_qlinear_power_utility = {x : 0 for x in range(sublist, end, Delta)}
+    average_qlinear_square_utility = {x : 0 for x in range(sublist, end, Delta)}
+    average_miscelaneous_1_utility = {x : 0 for x in range(sublist, end, Delta)}
+    average_miscelaneous_2_utility = {x : 0 for x in range(sublist, end, Delta)}
+    average_miscelaneous_3_utility = {x : 0 for x in range(sublist, end, Delta)}
+    average_exponential_utility = {x : 0 for x in range(sublist, end, Delta)}
+    average_s_shape_utility = {x : 0 for x in range(sublist, end, Delta)}
+    
+
+    average_oranks_students = {x : 0 for x in range(sublist, end, Delta)}
+    average_oranks_schools = {x : 0 for x in range(sublist, end, Delta)}
+    ranks_students = {x : [] for x in range(sublist, end, Delta)}
+    ranks_schools = {x : [] for x in range(sublist, end, Delta)}
+    r_profile = {x : np.zeros(n_schools+1) for x in range(sublist, end, Delta)}
+
+
+    for i in range(iterations):
+        print('Working on iteration: ' + str(i))
+
+        student_Match, school_Match, student_original_preferences, school_original_preferences = simulation_matching_increase_preferences_ttc(Delta, sublist, n_students, n_schools, additions)
+
+        nash_welfare_students, nash_welfare_schools = nash_welfare(student_Match, school_Match)
+        mean_original_ranks_students, mean_original_ranks_schools, or_students, or_schools = average_rank_match(student_Match, school_Match)
+        ranks_profile = rank_profile(student_Match, school_Match)
+        (leontief_u, cobb_stone_u, qlinear_power_u, qlinear_square_u, 
+        miscelaneous_1_u, miscelaneous_2_u, miscelaneous_3_u,
+        exponential_u, s_shape_u) = utility_functions(student_Match, school_Match, ranks_profile)
+
+
+        #Students
+        for size, value in nash_welfare_students.items():
+            average_nash_welfare_students[size] += value/iterations
+
+        for size, value in mean_original_ranks_students.items():
+            average_oranks_students[size] += value/iterations 
+
+        for size, ary in or_students.items():
+            ranks_students[size].extend(ary)
+        
+        for size, rk_profile in ranks_profile.items():
+            r_profile[size] = r_profile[size] + rk_profile/iterations  
+        
+        for size, value in leontief_u.items():
+            average_leontief_utility[size] += value/iterations
+
+        for size, value in cobb_stone_u.items():
+            average_cobb_stone_utility[size] += value/iterations
+
+        for size, value in qlinear_power_u.items():
+            average_qlinear_power_utility[size] += value/iterations
+
+        for size, value in qlinear_square_u.items():
+            average_qlinear_square_utility[size] += value/iterations
+
+        for size, value in miscelaneous_1_u.items():
+            average_miscelaneous_1_utility[size] += value/iterations
+        
+        for size, value in miscelaneous_2_u.items():
+            average_miscelaneous_2_utility[size] += value/iterations
+
+        for size, value in miscelaneous_3_u.items():
+            average_miscelaneous_3_utility[size] += value/iterations
+        
+        for size, value in exponential_u.items():
+            average_exponential_utility[size] += value/iterations
+
+        for size, value in s_shape_u.items():
+            average_s_shape_utility[size] += value/iterations 
+
+        #Schools
+
+        for size, value in nash_welfare_schools.items():
+            average_nash_welfare_schools[size] += value/iterations
+
+        for size, value in mean_original_ranks_schools.items():
+            average_oranks_schools[size] += value/iterations
+
+        for size, ary in or_schools.items():
+            ranks_schools[size].extend(ary)
+
+    return (average_nash_welfare_students, average_nash_welfare_schools, 
+    average_oranks_students, average_oranks_schools,
+    ranks_students, ranks_schools, 
+    r_profile, 
+    average_leontief_utility, average_cobb_stone_utility, 
+    average_qlinear_power_utility, average_qlinear_square_utility, 
+    average_miscelaneous_1_utility, average_miscelaneous_2_utility,
+    average_miscelaneous_3_utility, average_exponential_utility,
+    average_s_shape_utility)
+
+
+##################################################################
+## Pareto Optimality in Gale Shapley output
+##################################################################
+
+def trade_in_free(student_match, school_match, student_prefs):
+    school_mat = np.array(school_match[:, 1]) 
+    unmatched_schools = np.where(school_mat == -9999)
+    trade_free = True
+    for student in range(len(student_match)):
+        student_preferences = student_prefs[student]
+        for school in unmatched_schools:
+            potential_match = student_preferences[student_preferences[:, 1] == school][0]
+            if len(potential_match)>0:
+                if student_match[student, 0] > potential_match[0]:
+                    trade_free = False 
+                    break 
+        if trade_free == False: 
+            break
+
+    return trade_free
+
+def coalition_free(student_match, school_match, student_prefs):   
+    coalition_f = True
+
+    n_students = len(student_match)
+    n_schools = len(school_match)
+
+    candidate_students = list(range(n_students))
+    candidate_schools = list(range(n_schools))
+
+    next_student_choice = [0] * n_students
+
+    first_on_list = True
+    while (first_on_list==True and len(candidate_students)>0):
+        for student in candidate_students:
+            student_pref  = np.copy(student_prefs[student])
+            if next_student_choice[student] < student_pref.shape[0]:
+                sch = student_pref[next_student_choice[student]][1]
+                if sch in candidate_schools:
+                    if student_match[student][1] == sch:
+                        candidate_students.remove(student)
+                        candidate_schools.remove(sch)
+                    else: 
+                        first_on_list = False
+                else:
+                    next_student_choice[student] += 1
+            else:
+                candidate_students.remove(student)
+
+
+    if len(candidate_students)>0:
+
+        visited = set()
+        cycles = []
+
+        for start_student in candidate_students:
+            
+            if start_student in visited:
+                continue
+
+            path_students = [start_student]
+            student_pref  = student_prefs[start_student]
+            school = student_pref[next_student_choice[start_student]][1]
+            path_schools = [school]
+            next_student = school_match[school]
+
+            loop_break = 0
+            while next_student not in path_students:
+                if next_student in visited:
+                    loop_break = 1
+                    break
+                else:
+                    path_students.append(next_student)
+                    student_pref  = student_prefs[next_student]
+                    next_school = student_pref[next_student_choice[next_student]][1]
+                    path_schools.append(next_school)
+                    next_student = school_match[next_school]
+            
+            if loop_break == 0:
+                cycle = []
+                for stud in reversed(path_students):
+                    cycle.insert(0,stud)
+                    if stud == next_student:
+                        break
+                cycles.append(cycle)
+                visited |= set(path_students)
+            else:
+                visited |= set(path_students)
+        
+        if len(cycles)>0:
+            for cycle in cycles:
+                if len(cycle)>1:
+                    coalition_f = False
+                    break 
+
+    return coalition_f
+
+
+def is_pareto_optimal(student_match, school_match, student_prefs):
+    
+    pareto = False
+
+    t_in_free = trade_in_free(student_match, school_match)
+
+    if t_in_free == True: 
+        c_free = coalition_free(student_match, school_match, student_prefs)
+        if c_free == True:
+            pareto = True
+        
+    return pareto
 
