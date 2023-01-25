ea8d0897b6f1572f599e7197a9921faef427e1de
danielsloredo <daniel.sloredo@gmail.com>
2023-01-25 14:31:35 -0500

    corrections to serial dictatorship approximation

--- Code/approx_rank.py
+++ Code/approx_rank.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 from scipy.stats import hypergeom
 from itertools import product
-
+'''
 def expected_rank_sublist_student_prev_approx(sublist, student, n_schools):
     expected_rank = 0
     sum_to_overlap = np.min([sublist + 1, student])
@@ -107,7 +107,7 @@ def simulation_ranks(initial_sublist, additions, delta, n_students, n_schools):
         nash_welfare_students[sublist_size] = nash_welfare(ranks_students_size, n_schools)
     
     return ranks_students, average_oranks_students, nash_welfare_students
-
+'''
 #############################################################################################
 #############################################################################################
 #Approximation Formula
@@ -127,40 +127,86 @@ def expected_min_rank_overlaps(sublist, student, matched, n_schools):
         overlap_prob[overlap] = hypergeom.pmf(overlap, n_schools, sublist, matched)
 
     sum_expected_overlap = np.sum(expected_min * overlap_prob)
-
     return sum_expected_overlap
 
 def probability_matched(sublist, student, matched, n_schools):
     
     w_d_l = np.zeros(matched - sublist)
     
-    for l in range(sublist, matched):
-        w_d_l = 1 - math.comb(n_schools - sublist, l - sublist)/math.comb(n_schools, l)
-    
-    product_w_d_l = np.product(w_d_l)
-
-    t_values_function = lambda n,t:[k for k in product(range(1, t+1),repeat=n) if sum(k)==t]
-
-    t_values = t_values_function(matched-sublist+1, student - matched)
-
-    if len(t_values) == 0:
-        path_probs_sum = 1
+    if len(w_d_l) == 0:
+        product_w_d_l = 1
     else:
-        path = 0
-        path_probability = np.zeros(len(t_values))
-        for t_values_path in t_values:
-            step_prob = np.zeros(matched - sublist + 1)
-            for j in range(sublist, matched):
-                step_prob[j - sublist] = math.comb(n_schools - sublist, j - sublist)/math.comb(n_schools, j)
-            path_probabilities = np.power(step_prob, t_values_path)
-            path_probability[path] = np.prod(path_probabilities)
-            path += 1
-        path_probs_sum = np.sum(path_probabilities)
+        for l in range(sublist, matched):
+            w_d_l[l-sublist] = 1 - hypergeom.pmf(sublist, n_schools, sublist, l)
+        product_w_d_l = np.product(w_d_l)
+
+    t_values_function = lambda n,t:[k for k in product(range(t+1),repeat=n) if sum(k)==t]
+
+    t_values = t_values_function(matched-sublist+1, student - 1 - matched)
+
+    path = 0
+    path_probability = np.zeros(len(t_values))
+    for t_values_path in t_values:
+        step_prob = np.zeros(matched - sublist + 1)
+        for j in range(sublist, matched + 1):
+            step_prob[j - sublist] = hypergeom.pmf(sublist, n_schools, sublist, j)
+        path_probabilities = np.power(step_prob, t_values_path)
+        path_probability[path] = np.prod(path_probabilities)
+        path += 1
+
+    path_probs_sum = np.sum(path_probability)
+    
     
     probability_k_match = product_w_d_l * path_probs_sum
-
+    #print('The probability diagonal for mathced '+str(matched)+' for student '+str(student)+' is of '+str(product_w_d_l))
+    #print('The probability path for matched '+str(matched)+' for student '+str(student)+' is of '+str(path_probs_sum))
+    #print('The probability of matched '+str(matched)+' for student '+str(student)+' is of '+str(probability_k_match))
     return probability_k_match
 
+def expected_rank_student_sublist(sublist, student, n_schools):
+    E_rank_i_d = 0
+
+    if student-1 <= sublist:
+        E_rank_i_d = expected_min_rank_overlaps(sublist, student, student-1, n_schools)
+    else:
+        for k in range(sublist, student):
+            E_rank_i_d += probability_matched(sublist,student, k, n_schools) * expected_min_rank_overlaps(sublist, student, k, n_schools)
+    #print('The expected rank for  '+str(student)+' is of '+ str(E_rank_i_d))
+    return E_rank_i_d
 
 
+def expected_ranks_sublist(sublist, n_students, n_schools):
+    index_students = list(range(n_students)) 
+    expected_ranks = np.zeros(n_students) 
 
+    expected_ranks[0] = (n_schools+1)/(sublist+1)
+
+    for index_std in index_students[1:]:
+        expected_ranks[index_std] = expected_rank_student_sublist(sublist, index_std+1, n_schools)
+
+    return expected_ranks
+
+def nash_welfare(student_ranks, n_schools): 
+
+    ranks = np.copy(student_ranks)
+    utility = np.array(n_schools+2-ranks, dtype=float)
+    nash_welfare_students = np.power(utility.prod(),(1/student_ranks.shape[0]))
+        
+    return nash_welfare_students
+
+def simulation_ranks(initial_sublist, additions, delta, n_students, n_schools):
+    
+    end = initial_sublist+delta*additions + delta
+
+    ranks_students = {}
+    average_oranks_students = {x : 0 for x in range(initial_sublist, end, delta)}
+    nash_welfare_students = {x : 0 for x in range(initial_sublist, end, delta)}
+    
+    for sublist_size in range(initial_sublist, end, delta):
+        print('working on sublist size: ' + str(sublist_size))
+        ranks_students_size = expected_ranks_sublist(sublist_size, n_students, n_schools)
+        ranks_students[sublist_size] = ranks_students_size
+        average_oranks_students[sublist_size] = ranks_students_size.mean()
+        nash_welfare_students[sublist_size] = nash_welfare(ranks_students_size, n_schools)
+    
+    return ranks_students, average_oranks_students, nash_welfare_students
